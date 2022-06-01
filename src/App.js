9ebddfa886514006867dd1ccea8f31998d0f8fc8
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-06-01 16:50:20 +0600

    use google popup sin in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const auth = getAuth(app)
 
 function App() {
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSinIn = () =>{
+    signInWithPopup(auth, provider)
+    .then(result =>{
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error =>{
+      console.log(error);
+    })
+  }
   return (
     <div className="App">
-     
+     <button onClick={handleGoogleSinIn}>Google Sin In</button>
     </div>
   );
 }
